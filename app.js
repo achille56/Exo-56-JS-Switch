437fa81493bc943003d7986939541c4fa2e5c466
achille56 <leclercqjm59@gmail.com>
2022-11-20 11:18:32 +0100

    exo 56

---
+++ app.js
@@ -0,0 +1,24 @@
+let accessoireInf = prompt("Nom d'accessoire");
+
+switch (accessoireInf) {
+    case 'souris':
+        alert("j'utilise une souris");
+        break;
+    case 'clavier':
+        alert("j'utilise un clavier");
+        break;
+    case 'ordinateur':
+        alert("j'utilise un ordinateur");
+        break;
+    case 'tapis':
+        alert("j'utilise un tapis");
+        break;
+    case 'imprimante':
+        alert("j'utilise une imprimante");
+        break;
+    case 'ondulateur':
+        alert("j'utilise un ordinateur");
+        break;
+    default:
+        alert("accessoire inconnu");
+}
